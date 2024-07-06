289730d64e51782bc221f6b1d5098c56eb867ff0
nocheol <qkdl60@gmail.com>
2024-07-07 00:50:51 +0900

    feat : 정렬 기능 추가 및 카드 컴포넌트 적용

--- qkdl60/src/App.tsx
+++ qkdl60/src/App.tsx
@@ -1,18 +1,61 @@
 /* 여기에 주어진 요구 사항을 충족 시키기 위한 코드를 작성 및 수정해 주세요. */
 import "./App.css";
-import Table from "./components/Table";
+import Card from "./components/Card";
+
+import {useEffect, useState} from "react";
+
+type OrderType = "latest" | "views";
+
+const isOrderType = (value: any): value is OrderType => {
+  return value === "latest" || value === "views";
+};
+type dataString = `${number}-${number}-${number}`;
+export interface Post {
+  title: string;
+  views: number;
+  upload_date: string;
+  bookmark: dataString;
+}
+const sortList = (orderType: OrderType, dataList: Post[]) => {
+  if (orderType === "latest") return dataList.sort((a, b) => (a.upload_date < b.upload_date ? 1 : -1));
+  if (orderType === "views") return dataList.sort((a, b) => b.views - a.views);
+  return dataList;
+};
 
 function App() {
+  const [orderState, setOrderState] = useState<OrderType>("latest");
+  const [postList, setPostList] = useState<Post[]>([]);
+  const sortedList = sortList(orderState, postList);
+
+  useEffect(() => {
+    (async () => {
+      const res = await fetch("./data/posts.json");
+      if (res.ok) {
+        const data: Post[] = await res.json();
+        setPostList(data);
+      }
+    })();
+  }, []);
+
+  const handleChangeOrderType = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const nextOrderState = event.target.value;
+    if (isOrderType(nextOrderState) && orderState !== nextOrderState) {
+      setOrderState(nextOrderState);
+    }
+  };
+
   return (
     <div className="container">
       <div className="section __order">
-        <select id="order_type">
-          <option value="1">최근등록순</option>
-          <option value="2">조회순</option>
+        <select id="order_type" onChange={handleChangeOrderType}>
+          <option value="latest">최근등록순</option>
+          <option value="views">조회순</option>
         </select>
       </div>
       <div className="section">
-        <Table />
+        {sortedList.map((post, index) => (
+          <Card key={index} title={post.title} upload_date={post.upload_date} views={post.views} bookmark={post.bookmark} />
+        ))}
       </div>
     </div>
   );
--- qkdl60/src/components/Card.tsx
+++ qkdl60/src/components/Card.tsx
@@ -1,12 +1,13 @@
 /* 여기에 주어진 요구 사항을 충족 시키기 위한 코드를 작성 및 수정해 주세요. */
 import "./Card.css";
-
-function Card() {
+import {Post} from "../App";
+//TODO 북마크 처리
+function Card({title, views, upload_date, bookmark}: Post) {
   return (
     <li className="card--container" id="card1">
       <div className="header">
         <div className="card--tag">
-          <span className="upload-date">2024-11-09</span>
+          <span className="upload-date">{upload_date}</span>
         </div>
         <div className="card--tag">
           <span className="icon bookmark">
@@ -15,11 +16,11 @@ function Card() {
         </div>
       </div>
       <div className="card--content">
-        <span className="title">Quick Brown Fox</span>
+        <span className="title">{title}</span>
       </div>
       <div className="footer">
         <div className="card--tag">
-          <span className="views">73</span>
+          <span className="views">{views}</span>
           views
         </div>
       </div>
