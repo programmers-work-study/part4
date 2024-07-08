8f10fd2ac218fc4be6105a610b9dc08e1de65b9f
seeys <es2008e@naver.com>
2024-07-08 20:11:22 +0900

    게시물 정렬

--- whatseni/src/App.tsx
+++ whatseni/src/App.tsx
@@ -1,5 +1,5 @@
 /* 여기에 주어진 요구 사항을 충족 시키기 위한 코드를 작성 및 수정해 주세요. */
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import Table from "./components/Table";
 import { json } from "stream/consumers";
@@ -16,6 +16,14 @@ function App() {
   const [data, setData] = useState<IData[] | null>(null);
   const [bySort, setBySort] = useState<SortType>('recent');
 
+  const handleChangeType = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    if (event.target.value === '1') {
+      setBySort('recent');
+    } else if (event.target.value === '2') {
+      setBySort('view')
+    }
+  }
+
   const fetchData = async () => {
     try {
       const response = await fetch("./data/posts.json");
@@ -26,18 +34,30 @@ function App() {
     }
   }
 
+  const sortData = (list: IData[] | null) => {
+    if (!list) return;
+
+    let sortedData;
+    if (bySort === 'recent') {
+      sortedData = list.sort((a: IData, b: IData) => new Date(b.upload_date).getTime() - new Date(a.upload_date).getTime());
+    } else if (bySort === 'view') {
+      sortedData = list.sort((a: IData, b: IData) => b.views - a.views);
+    }
+    setData(sortedData)
+  }
+
   useEffect(() => {
     fetchData().then(res => {
       if (res) {
-        setData(res);
+        sortData(res);
       }
     })
-  }, []);
+  }, [bySort]);
 
   return (
     <div className="container">
       <div className="section __order">
-        <select id="order_type">
+        <select id="order_type" onChange={handleChangeType}>
           <option value="1">최근등록순</option>
           <option value="2">조회순</option>
         </select>
