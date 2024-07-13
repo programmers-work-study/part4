a0032a485e4b0cff7641ef6aebeaa0c2c7c95470
nocheol <qkdl60@gmail.com>
2024-07-13 16:20:22 +0900

    refactor : data 위치 변경, sort

--- qkdl60/src/App.tsx
+++ qkdl60/src/App.tsx
@@ -1,18 +1,18 @@
 /* 여기에 주어진 요구 사항을 충족 시키기 위한 코드를 작성 및 수정해 주세요. */
 import "./App.css";
 import Card from "./components/Card";
-import {useEffect, useState} from "react";
-
+import {useState} from "react";
+import data from "./data/posts.json";
 type OrderType = "latest" | "views";
 
 const isOrderType = (value: any): value is OrderType => {
   return value === "latest" || value === "views";
 };
-type dataString = `${number}-${number}-${number}`;
+
 export interface Post {
   title: string;
   views: number;
-  upload_date: dataString;
+  upload_date: string;
   bookmark: boolean;
 }
 const sortList = (orderType: OrderType, dataList: Post[]) => {
@@ -23,6 +23,9 @@ const sortList = (orderType: OrderType, dataList: Post[]) => {
       }
       const dateA = new Date(a.upload_date);
       const dateB = new Date(b.upload_date);
+      const dateATime = dateA.getTime();
+      const dateBTime = dateB.getTime();
+      if (dateATime === dateBTime) return a.title < b.title ? -1 : 1;
       return dateB.getTime() - dateA.getTime();
     });
   }
@@ -31,6 +34,7 @@ const sortList = (orderType: OrderType, dataList: Post[]) => {
       if (a.bookmark !== b.bookmark) {
         return a.bookmark ? -1 : 1;
       }
+      if (a.views === b.views) return a.title < b.title ? -1 : 1;
       return b.views - a.views;
     });
   return dataList;
@@ -38,18 +42,8 @@ const sortList = (orderType: OrderType, dataList: Post[]) => {
 
 function App() {
   const [orderState, setOrderState] = useState<OrderType>("latest");
-  const [postList, setPostList] = useState<Post[]>([]);
+  const [postList, setPostList] = useState<Post[]>(data);
   const sortedList = sortList(orderState, postList);
-
-  useEffect(() => {
-    (async () => {
-      const res = await fetch("./data/posts.json");
-      if (res.ok) {
-        const data: Post[] = await res.json();
-        setPostList(data);
-      }
-    })();
-  }, []);
 
   const handleChangeOrderType = (event: React.ChangeEvent<HTMLSelectElement>) => {
     const nextOrderState = event.target.value;
